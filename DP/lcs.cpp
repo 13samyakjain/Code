4c0deee4e2324f97541640a668f251409481d24a
13samyakjain <samyakjain0113@gmail.com>
2022-03-22 23:55:23 +0530

    add

--- DP/lcs.cpp
+++ DP/lcs.cpp
@@ -7,6 +7,8 @@ class solution{
         int lcs(string s,string t){
             int n = s.size();
             int m = t.size();
+
+        //for minimum operations to make palindrome, sub string length - longest palindromic seq length
             vector<vector<int>> dp(n+1,vector<int>(m+1,0));
             for(int i=0;i<=n;i++) dp[i][0] = 0;
             for(int j=0;j<=m;j++) dp[0][j] = 0;
