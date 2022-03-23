2f37d842c3bd44a32b7d3fa824b5c02c2355ea67
13samyakjain <samyakjain0113@gmail.com>
2022-03-23 18:28:32 +0530

    trapping water

---
+++ Arrays/trappingrainwater.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+      int left=0,right=height.size()-1,leftmax=0,rightmax=0,res=0;  
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>=leftmax){
+                    leftmax=height[left];
+                }else{
+                    res+=leftmax-height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>=rightmax){
+                    rightmax=height[right];
+                }else{
+                    res+=rightmax-height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
+};
